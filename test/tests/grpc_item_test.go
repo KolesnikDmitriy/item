e85d72d99f4d9059d990e0d394c8a3f802f5c091
Dima Kolesnik <dmitrykolesnik1@gmail.com>
2021-09-24 14:37:12 +0300

    t.Parallel added

--- test/tests/grpc_item_test.go
+++ test/tests/grpc_item_test.go
@@ -39,6 +39,8 @@ func TestItemGRPC(t *testing.T) {
 		for _, tc := range testCases {
 			tc := tc
 			t.Run(tc.name, func(t *testing.T) {
+				t.Parallel()
+
 				_, err := api.Item(&tc.req)
 
 				require.Error(t, err)
